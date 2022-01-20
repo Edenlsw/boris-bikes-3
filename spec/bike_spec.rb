507517c4171105682eddff9d20cd7f3a285cac05
Eden <elsw@hotmail.co.uk>
2022-01-20 14:36:43 +0000

    updated bike respond_to method

--- spec/bike_spec.rb
+++ spec/bike_spec.rb
@@ -3,7 +3,12 @@ require './lib/Bike.rb'
 describe Bike do 
   it "checks if the bike is working" do 
     bike = Bike.new
-    expect(bike.working?).to respond_to
+    # expect(bike.working?).to respond_to
+
+    expect(bike).to respond_to(:working?)
+
+
+
 
 
   end
